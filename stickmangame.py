77cd7f5853da97273c7babfd84a7f60b7fbf3704
boris <bobozenn@gmail.com>
2021-09-22 15:07:11 +0300

    Fourth

--- stickmangame.py
+++ stickmangame.py
@@ -2,13 +2,14 @@ from tkinter import *
 import random
 import time
 
+
 class Game:
     def __init__(self):
         self.tk = Tk()
         self.tk.title("Господин Клечка тича към изхода")
         self.tk.resizable(0, 0)
         self.tk.wm_attributes("-topmost", 1)
-        self.canvas = Canvas(self.tk, width=500, heighth=500, highlightthickness=0)
+        self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
         self.canvas.pack()
         self.tk.update()
         self.canvas_height = 500
@@ -18,15 +19,82 @@ class Game:
         h = self.bg.height()
         for x in range(0, 5):
             for y in range(0, 5):
-                self.canvas.create_image(x*w, y*h, image=self.bg, anchor='nw')
-            self.sprites = []
-            self.running = True
+                self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
+        self.sprites = []
+        self.running = True
 
-        def mainloop(self):
-            while 1:
-                if self.running == True:
-                    for srite in self.sprites:
-                        sprite.move()
-                        self.tk.update_idletasks()
-                        self.tk.update()
-                        time.sleep(0.01)
+    def main_loop(self):
+        while 1:
+            if self.running:
+                for sprite in self.sprites:
+                    sprite.move()
+                self.tk.update_idletasks()
+                self.tk.update()
+                time.sleep(0.01)
+
+
+class Coords:
+    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+
+class Sprite:
+    def __init__(self, game):
+        self.game = game
+        self.endgame = False
+        self.coordinates = False
+    def move(self):
+        pass
+    def coords(self):
+        self.coordinates
+
+def within_x(co1, co2):
+    if (co2.x1 < co1.x1 < co2.x2) \
+            or (co2.x1 < co1.x2 < co2.x2) \
+            or (co1.x1 < co2.x1 < co1.x2) \
+            or (co1.x1 < co2.x2 < co1.x2):
+        return True
+    else:
+        return False
+
+
+def within_y(co1, co2):
+    if (co2.y1 < co1.y1 < co2.y2) \
+            or (co2.y1 < co1.y2 < co2.y2) \
+            or (co1.y1 < co2.y1 < co1.y2) \
+            or (co1.y1 < co2.y2 < co1.y2):
+        return True
+    else:
+        return False
+
+
+def collided_left(co1, co2):
+    if within_y(co1, co2) and co2.x1 <= co1.x1 <= co2.x2:
+        return True
+    return False
+
+
+def collided_right(co1, co2):
+    if within_y(co1, co2) and co2.x1 <= co1.x2 <= co2.x2:
+        return True
+    return False
+
+
+def collided_top(co1, co2):
+    if within_x(co1, co2) and co2.y1 <= co1.y1 <= co2.y2:
+        return True
+    return False
+
+
+def collided_bottom(y, co1, co2):
+    y_calc = co1.y2 + y
+    if within_x(co1, co2) and co2.y1 <= y_calc <= co2.y2:
+        return True
+    return False
+
+
+g = Game()
+g.main_loop()
